46a3ee9276e9ae03613621182f810d1e8a866a17
dansimiy2906 <daniel.simiyu@strathmore.edu>
2021-10-15 16:12:50 +0300

    first commit

---
+++ Assets/Gunrigidbody.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gunrigidbody : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject myPrefab;
+    public float movespeed = 1.0f;
+    public float turnSpeed = 1.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if(Input.GetKey("a")){
+    		this.transform.position = this.transform.position + Vector3.forward * movespeed;
+    	}
+    	if(Input.GetKey("s")){
+    		Instantiate(myPrefab, new Vector3(0,0,0), Quaternion.identity);
+    		Destroy(myPrefab,3);
+
+    	}
+        
+    }
+}
